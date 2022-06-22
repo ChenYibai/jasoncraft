026f6b88700492ed09e2f109b87f65f6c7699b90
ChenYibai <mosesb@126.com>
2022-06-22 23:03:13 +0800

    0.1.1 Inventory & Compatibility

--- img/blockdata.js
+++ img/blockdata.js
@@ -1,5 +1,5 @@
-// blockData[id] = [name, posInSprite: [x, y]]
-const blockData = [["Grass Block", [12, 26]],
-["Dirt", [2, 0]],
-["Stone", [42, 25]],
-["Iron Ore", [0, 29]]];
+// blockData[id] = [name, posInSprite: [x, y], dropItemID(or -1)]
+const blockData = [["Grass Block", [12, 26], 0],
+["Dirt", [2, 0], 1],
+["Stone", [42, 25], 2],
+["Iron Ore", [0, 29], 3]];
---
+++ img/itemdata.js
@@ -0,0 +1,5 @@
+// itemData[ID] = [name, posInSprite: [x, y], associatedBlockID(or -1)]
+const itemData = [["Grass Block", [20, 30], 0],
+["Dirt", [18, 30], 1],
+["Stone", [7, 31], 2],
+["Iron Ore", [2, 106], 3]];
